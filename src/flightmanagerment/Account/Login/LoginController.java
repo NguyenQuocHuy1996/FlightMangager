9c6e9e199cd2563fb537b4ff84d87e817de18ea4
QuocHuy <shermeilove@gmail.com>
2018-03-06 16:28:10 +0700

    Inital_Project_v1.0

---
+++ src/flightmanagerment/Account/Login/LoginController.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flightmanagerment.Account.Login;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Quoc Huy
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private TextField txtEmail;
+    @FXML
+    private TextField txtPass;
+
+    @FXML
+    private void Login(ActionEvent event) throws IOException {
+        //Load new windown and hide current windown
+        Parent root = FXMLLoader.load(getClass().getResource("/flightmanagerment/Home/HomeUI.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.hide();
+        stage.setScene(scene);
+        stage.setTitle("Home");
+        stage.show();
+        
+        //Load new windown
+//        FXMLLoader fxmlLoader = new FXMLLoader();
+//        fxmlLoader.setLocation(getClass().getResource("/flightmanagerment/Home/HomeUI.fxml"));
+//        //Set new Scene
+//        Scene scene = new Scene(fxmlLoader.load());
+//        
+//        //Set new Stage
+//        Stage stage = new Stage();        
+//        stage.setTitle("New Window");
+//        stage.setScene(scene);
+//        
+//        //Show new windown
+//        stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
---
+++ src/flightmanagerment/Home/HomeController.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flightmanagerment.Home;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author Quoc Huy
+ */
+public class HomeController implements Initializable {
+
+//    @FXML
+//    private TextField txtEmail;
+//    @FXML
+//    private TextField txtPass;
+//    
+//    @FXML
+//    private void Home(ActionEvent event) {
+//        if((txtEmail.getText().equals("admin")) && (txtPass.getText().equals("admin123"))){
+//            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+//            alert.setTitle("Dang nhap thanh cong");
+//            alert.show();
+//        }
+//    }
+    
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
