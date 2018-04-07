9b671b1fb254d525e0de4e610d4171c583e4a3b7
pnxphuc <pnxphuc96@gmail.com>
2018-04-08 00:35:59 +0700

    fix function ChangePassword in ChangePassController

--- src/flightmanagerment/Form/Account/ChangePass/ChangePassController.java
+++ src/flightmanagerment/Form/Account/ChangePass/ChangePassController.java
@@ -7,6 +7,7 @@ package flightmanagerment.Form.Account.ChangePass;
 
 import flightmanagerment.Function.AccountDAO;
 import flightmanagerment.Model.Variable_Static;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
@@ -14,11 +15,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -47,7 +51,7 @@ public class ChangePassController implements Initializable {
 //        }
 //    }
     @FXML
-    private void btn_changedPassword(ActionEvent event) throws SQLException {
+    private void btn_changedPassword(ActionEvent event) throws SQLException, IOException {
         Alert a = new Alert(Alert.AlertType.ERROR);
         if (old_password.getText().equals("") || (new_password.getText().equals("")) || (confirmed_password.getText().equals(""))) {
             a.setTitle("ERROR");
@@ -57,19 +61,45 @@ public class ChangePassController implements Initializable {
             if (new_password.getText().equals(confirmed_password.getText())) {
                 int function = AccountDAO.checkRole(Variable_Static.USERNAME);
                 if (function == 1) {
-                    AccountDAO.changePasswordCus(old_password.getText(), new_password.getText(), Variable_Static.USERNAME);
-                    Alert.AlertType type = Alert.AlertType.INFORMATION;
-                    a.setAlertType(type);
-                    a.setTitle("Đổi mật khẩu thành công!");
-                    a.setContentText("Bạn đã đổi mật khẩu thành công!");
-                    a.show();
+                    if (AccountDAO.changePasswordCus(old_password.getText(), new_password.getText(), Variable_Static.USERNAME) == 1) {
+                        Alert.AlertType type = Alert.AlertType.INFORMATION;
+                        a.setAlertType(type);
+                        a.setTitle("Đổi mật khẩu thành công!");
+                        a.setContentText("Bạn đã đổi mật khẩu thành công!");
+                        a.show();
+                        
+                        Parent root = FXMLLoader.load(getClass().getResource("/flightmanagerment/Form/Home/MainUser/MainUserUI.fxml"));
+                        Scene scene = new Scene(root);
+                        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                        stage.hide();
+                        stage.setScene(scene);
+                        stage.setMaximized(true);
+                        stage.resizableProperty().setValue(Boolean.FALSE);
+                        stage.setTitle("Main Customer ");
+                        stage.show();
+                    } else {
+                    }
+
                 } else {
-                    AccountDAO.changePasswordEmp(old_password.getText(), new_password.getText(), Variable_Static.USERNAME);
-                    Alert.AlertType type = Alert.AlertType.INFORMATION;
-                    a.setAlertType(type);
-                    a.setTitle("Đổi mật khẩu thành công!");
-                    a.setContentText("Bạn đã đổi mật khẩu thành công!");
-                    a.show();
+                    if (AccountDAO.changePasswordEmp(old_password.getText(), new_password.getText(), Variable_Static.USERNAME) == 1) {
+                        Alert.AlertType type = Alert.AlertType.INFORMATION;
+                        a.setAlertType(type);
+                        a.setTitle("Đổi mật khẩu thành công!");
+                        a.setContentText("Bạn đã đổi mật khẩu thành công!");
+                        a.show();
+                        
+                        Parent root = FXMLLoader.load(getClass().getResource("/flightmanagerment/Form/Home/MainStaff/MainStaffUI.fxml"));
+                        Scene scene = new Scene(root);
+                        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                        stage.hide();
+                        stage.setScene(scene);
+                        stage.setMaximized(true);
+                        stage.resizableProperty().setValue(Boolean.FALSE);
+                        stage.setTitle("Main Staff ");
+                        stage.show();
+                    } else {
+                    }
+
                 }
             } else {
                 Alert.AlertType type = Alert.AlertType.ERROR;
