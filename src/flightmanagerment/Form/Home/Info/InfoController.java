8c18bb5e2a7e5742ebe43f4266c9bdbc218b9108
pnxphuc <pnxphuc96@gmail.com>
2018-04-09 15:12:44 +0700

    combobox city + district in InfoUserController & combobox homeTown in InfoUserController

--- src/flightmanagerment/Form/Home/Info/InfoController.java
+++ src/flightmanagerment/Form/Home/Info/InfoController.java
@@ -5,16 +5,30 @@
  */
 package flightmanagerment.Form.Home.Info;
 
+import flightmanagerment.Function.CityDAO;
+import flightmanagerment.Function.CustomerDAO;
+import flightmanagerment.Function.DistrictDAO;
+import flightmanagerment.Model.City;
+import flightmanagerment.Model.Customer;
+import flightmanagerment.Model.Variable_Static;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -36,10 +50,6 @@ import javafx.stage.Stage;
 public class InfoController implements Initializable {
 
     @FXML
-    private TextField email;
-    @FXML
-    private PasswordField password;
-    @FXML
     private TextField firstName;
     @FXML
     private TextField lastName;
@@ -50,23 +60,23 @@ public class InfoController implements Initializable {
     @FXML
     private DatePicker dateOfBirth;
     @FXML
-    private RadioButton sex_male;
+    private RadioButton Male;
     @FXML
-    private RadioButton sex_female;
+    private RadioButton Female;
     @FXML
     private TextField add_Number;
     @FXML
     private TextField add_Street;
     @FXML
-    private ComboBox<?> homeTown;
+    private ComboBox<String> cbb_homeTown;
     @FXML
-    private ComboBox<?> add_City;
+    private ComboBox<String> cbb_City;
     @FXML
-    private ComboBox<?> add_District;
+    private ComboBox<String> cbb_District;
     @FXML
-    private ImageView _image;
+    private TextField email;
     @FXML
-    private ToggleGroup GroupRadioButton;
+    private PasswordField password;
 
 //    @FXML
 //    private TextField txtEmail;
@@ -83,45 +93,106 @@ public class InfoController implements Initializable {
 //    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }
+        ObservableList<String> list = CityDAO.getAllCity();
+        cbb_homeTown.setItems(list);
+        cbb_homeTown.getSelectionModel().select(1);
+        cbb_homeTown.setPromptText(cbb_homeTown.getConverter().toString(cbb_homeTown.getValue()));
 
-    @FXML
-    private void btn_Update(ActionEvent event) {
+        cbb_City.setItems(list);
+        cbb_City.getSelectionModel().select(1);
+        cbb_City.setPromptText(cbb_City.getConverter().toString(cbb_City.getValue()));
+
+        Customer cus = new Customer();
+        try {
+            cus = CustomerDAO.getCus(Variable_Static.USERNAME);
+            email.setText(cus.getEmail());
+            password.setText(cus.getPassword());
+            firstName.setText(cus.getFirstName());
+            lastName.setText(cus.getLastName());
+            ic_Card.setText(cus.getIc_Card());
+            if (cus.getDateOfBirth() != null) {
+                dateOfBirth.setValue(cus.getDateOfBirth().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+            }
+
+            if (cus.getSex()) {
+                Male.setSelected(true);
+                Female.setSelected(false);
+            } else {
+                Male.setSelected(false);
+                Female.setSelected(true);
+            }
+            cbb_homeTown.setValue(cus.getHomeTown());
+            cbb_City.setValue(cus.getAddress_city());
+            cbb_District.setValue(cus.getAddress_district());
+            // TODO
+        } catch (SQLException ex) {
+//            Logger.getLogger(InfoController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
     @FXML
     private void btn_Reset(ActionEvent event) {
+
     }
 
-    @FXML
     private void btn_Update1(ActionEvent event) {
-        phoneNumber.setEditable(true);
-        lastName.setEditable(false);
-        firstName.setEditable(true);
-        sex_male.setDisable(false);
-        sex_female.setDisable(false);
-        _image.setImage(new Image(""));
+//        phoneNumber.setEditable(true);
+//        lastName.setEditable(false);
+//        firstName.setEditable(true);
+//        sex_male.setDisable(false);
+//        sex_female.setDisable(false);
+//        _image.setImage(new Image(""));
     }
 
     @FXML
     private void btn_Update2(ActionEvent event) {
-        try {
-            Stage stage = new Stage();
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.setTitle("Open Resource File");
-            File file = fileChooser.showOpenDialog(stage);
-            if (file != null) {
-                System.out.println(file.getPath());
-                _image.setImage(new Image("..\\..\\..\\Asset\\img\\icon\\user.png"));
-            }
-        } catch (Exception e) {
-            throw e;
-        }
+        firstName.setEditable(true);
+        lastName.setEditable(true);
+        phoneNumber.setEditable(true);
+        ic_Card.setEditable(true);
+        dateOfBirth.setDisable(false);
+        Male.setDisable(false);
+        Female.setDisable(false);
     }
 
     @FXML
     private void btn_Update3(ActionEvent event) {
+        add_Number.setEditable(true);
+        add_Street.setEditable(true);
+        cbb_homeTown.setDisable(false);
+        cbb_City.setDisable(false);
+        cbb_District.setDisable(false);
+    }
+
+    @FXML
+    private void btn_Back(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/flightmanagerment/Form/Home/MainUser/MainUserUI.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.hide();
+        stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.resizableProperty().setValue(Boolean.FALSE);
+        stage.setTitle("Main Customer ");
+        stage.show();
+    }
+
+    @FXML
+    private void btn_ChooseCity(ActionEvent event) {
+        int maTinh = CityDAO.getMaTinh(cbb_City.getValue());
+
+        ObservableList<String> list = DistrictDAO.getDistrict(maTinh);
+        cbb_District.setItems(list);
+        cbb_District.getSelectionModel().select(1);
+        cbb_District.setPromptText(cbb_District.getConverter().toString(cbb_District.getValue()));
+
+    }
+    @FXML
+    private void btn_update(ActionEvent event) {
+        System.out.println("adb");
+        System.out.println("abc");
+        System.out.println("abcd");
     }
 
 }
--- src/flightmanagerment/Function/DistrictDAO.java
+++ src/flightmanagerment/Function/DistrictDAO.java
@@ -6,7 +6,6 @@
 package flightmanagerment.Function;
 
 import flightmanagerment.Config.ConnectDB;
-import flightmanagerment.Model.City;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -18,50 +17,32 @@ import javafx.collections.ObservableList;
  *
  * @author Admin
  */
-public class CityDAO {
+public class DistrictDAO {
 
     private static Connection conn = null;
     private static ConnectDB connectDB;
     private static PreparedStatement prest = null;
     private static ResultSet rs;
 
-    public static int getMaTinh(String TenTinh) {
-        connectDB = new ConnectDB();
-        conn = connectDB.getConnect();
-        try {
-            String sql = "select MaTinh from tinhthanh where TenTinh = ?";
-            prest = conn.prepareStatement(sql);
-            prest.setString(1, TenTinh);
-            rs = prest.executeQuery();
-            while (rs.next()) {                
-                int maTinh = rs.getInt("MaTinh");
-                return maTinh;
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        return 0;
-    }
-
-    public static ObservableList<String> getAllCity() {
+    public static ObservableList<String> getDistrict(int maTinh) {
         connectDB = new ConnectDB();
         conn = connectDB.getConnect();
         try {
             ArrayList<String> list = new ArrayList<String>();
-            String sql = "Select TenTinh from tinhthanh";
+            String sql = "select TenQuanHuyen from quanhuyen where MaTinh = ?";
             prest = conn.prepareStatement(sql);
+            prest.setInt(1, maTinh);
             rs = prest.executeQuery();
             ObservableList<String> listt = null;
             while (rs.next()) {
-//                int id = rs.getInt("id");
-//                int maTinh = rs.getInt("MaTinh");
-                String tenTinhthanh = rs.getString("TenTinh");
-                list.add(tenTinhthanh);
+                String tenQuanHuyen = rs.getString("TenQuanHuyen");
+                list.add(tenQuanHuyen);
             }
             listt = FXCollections.observableArrayList(list);
             return listt;
         } catch (Exception e) {
             System.out.println(e);
+            // TODO: handle exception
         }
         return null;
     }
