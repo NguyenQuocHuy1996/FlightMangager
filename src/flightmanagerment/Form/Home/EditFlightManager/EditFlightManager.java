8c7647f58d207b263b2bca3e41dbc5a5a46b533e
QuocHuy <shermeilove@gmail.com>
2018-04-14 16:22:20 +0700

    add form edit flight manager

---
+++ src/flightmanagerment/Form/Home/EditFlightManager/EditFlightManager.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flightmanagerment.Form.Home.EditFlightManager;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+/**
+ *
+ * @author Quoc Huy
+ */
+public class EditFlightManager extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("EditFlightManagerUI.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.resizableProperty().setValue(Boolean.FALSE);
+        stage.setTitle("Quản lý chuyến bay");
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
